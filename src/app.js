dc7e41dc8330f15f730b2386c862f9554649f319
Fabricio Arévalo Miranda <fabri_tkd@hotmail.com>
2021-08-22 22:55:47 -0500

    primera version

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express'),
+      path = require('path'),
+      morgan = require('morgan'),
+      mysql = require('mysql'),
+      myConnection = require('express-myconnection');
+
+const app = express();
+
+// importing routes
+const customerRoutes = require('./routes/customer');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(morgan('dev'));
+
+app.use(myConnection(mysql, {
+  host: 'bevvqmcdkeiiny8rgf0a-mysql.services.clever-cloud.com',
+  user: 'uaglzsmfmxkdliud',
+  password: 'XP08QOmTBrNeo91WmJhO',
+  port: 3306,
+  database: 'bevvqmcdkeiiny8rgf0a'
+
+}, 'single'));
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use('/', customerRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// starting the server
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
+});
